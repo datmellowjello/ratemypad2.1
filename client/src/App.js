5cfe62f2bf81a0c304695a8cd1fffa8706633811
Nancy Drew <49968896+Reeves93@users.noreply.github.com>
2019-10-16 21:54:29 -0600

    Add Place Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Results from "./pages/Results";
+import Place from "./pages/Place";
+
 import "./App.css";
 
-function App () {
+export default function App() {
   return (
-    <div>
-      <Results />
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={Results} />
+          <Route exact path="/places" component={Results} />
+          <Route exact path="/places/:id" component={Place} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <div className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h2>Welcome to React</h2>
-//         </div>
-//         <p className="App-intro">
-//           To get started, edit <code>src/App.js</code> and save to reload.
-//         </p>
-//       </div>
-//     );
-//   }
-// }
-
-export default App;
